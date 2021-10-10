f095ee31bbbce61ab6e02bd3457ec8b39b99f40e
Gokul Gopakumar <gokulgit@tutanota.com>
2021-10-10 20:44:10 +0530

    Added my first solidity program
    Gets three subjects from user and displays its sum and average

---
+++ code/firstProg.sol
@@ -0,0 +1,39 @@
+ 
+
+pragma solidity >=0.7.0 <0.9.0;
+
+ /*
+ * Simple program to ask for student details and marks. 
+ * Total and average of marks is calculated and displayed with student details.
+ */
+
+contract Storage {
+    
+    //State Variables
+    uint256 rollNumber; // Store the Roll Number of the Student
+    string name;       // Store the Name of the Student
+    uint256 mark1;
+    uint256 mark2;
+    uint256 mark3;
+    uint256 total;
+    uint256 avg;
+
+ 
+
+    function store(uint256 _rollNumber, string memory _name, uint256 _mark1, uint256 _mark2, uint256 _mark3) public {
+        
+        rollNumber = _rollNumber;
+        name = _name;
+        mark1 = _mark1;
+        mark2 = _mark2;
+        mark3 = _mark3;
+        total = _mark1 + _mark2 + _mark3;
+        avg = total/3;
+    }
+
+ 
+
+    function retrieve() public view returns (uint256, string memory, uint256, uint256){
+        return (rollNumber,name, total, avg);
+    }
+}
