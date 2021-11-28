b64668918478142ce7981eb929303e77a218dc11
Gokul Gopakumar <gokulgit@tutanota.com>
2021-11-28 17:43:28 +0530

    Added solidity program with struct

---
+++ code/Storage.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0  <0.9.0;
+
+contract Storage {
+    uint256 number;
+    string name;
+
+    function store(uint256 _num, string memory _name) public {
+        number = _num;
+        name = _name;
+    }
+
+    function retrieve () public view returns (uint256, string memory) {
+        return (number, name);
+
+    }
+}
+
+
+
